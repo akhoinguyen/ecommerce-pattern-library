ca66c3cbf33c447a2905c8f040d3401e07b6a063
Khoi Nguyen <43136539+akhoinguyen@users.noreply.github.com>
2020-03-09 21:06:34 -0400

    Fix footer link target area, add dim background to body when toggle nav, change wrapper-70 length plus minor tweaks

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,5 @@
 $('.nav-btn').on('click', function (e) {
   $('.nav-btn').toggleClass('nav-btn-active');
   $('.nav-links').toggleClass('nav-visible');
+  $('header ~ div.absolute').toggleClass('toggle-nav-dim');
 });
