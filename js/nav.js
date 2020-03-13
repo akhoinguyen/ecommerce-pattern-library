a2f334396dd9e5f4b40ed39b654e2e244298159b
Khoi Nguyen <43136539+akhoinguyen@users.noreply.github.com>
2020-03-13 13:50:48 -0400

    Comment out product card details list, add filter toggle on small screen

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,10 @@
 $('.nav-btn').on('click', function (e) {
   $('.nav-btn').toggleClass('nav-btn-active');
   $('.nav-links').toggleClass('nav-visible');
-  $('header ~ div.absolute').toggleClass('toggle-nav-dim');
+  $('header ~ div.absolute').toggleClass('toggle-bg-dim');
+});
+
+$('.filter-btn').on('click', function (e) {
+  $('.filter-wrap').toggleClass('filter-visible');
+  $('.filter-wrap ~ div.absolute').toggleClass('toggle-bg-dim');
 });
